09cdf566aa73d4f806de30d71b67f32f3dc39ec7
Jose Andres <joseandres216@gmail.com>
2020-08-09 22:15:24 -0600

    First update to frontend
    Done the first update for the frontend

--- railspotfe/src/AppComponents/AdminLogIn.js
+++ railspotfe/src/AppComponents/AdminLogIn.js
@@ -1,10 +1,18 @@
 import React, {Component} from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class AdminLogIn extends Component{
     render() {
         return(
             <div>
-                AL
+                <div>
+                    <TextField required={true} placeholder={"Usuario"} id="filled-basic" label="Usuario" variant="outlined" />
+                    <TextField required={true} placeholder={"Contraseña"} id="filled-basic" label="Contraseña" variant="outlined" />
+                </div>
+                <div>
+                    <Button variant="outlined">Iniciar sesion</Button>
+                </div>
             </div>
         );
     }
