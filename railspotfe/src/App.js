5e7dceb5f1b05e80391aee8cb4d7ce90ea27d877
Jose Andres <joseandres216@gmail.com>
2020-08-09 19:54:54 -0600

    Conversion from function to class
    Converted the App component to class to start working

--- railspotfe/src/App.js
+++ railspotfe/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+        Commit de conversion a clase
+      </div>
+    );
+  }
 }
+
 
 export default App;
