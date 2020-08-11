a91a432107a42af9a52ddf9a520be31a07cf4dc5
Josue Quesada <josuequesadaxd@gmail.com>
2020-08-10 21:52:13 -0600

    Consult Ticket UI

--- railspotfe/src/AppComponents/ConsultTickets.js
+++ railspotfe/src/AppComponents/ConsultTickets.js
@@ -1,15 +1,98 @@
 import React, {Component} from "react";
 import Divider from "@material-ui/core/Divider";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import {Grid} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+
 
 class ConsultTickets extends Component{
+    constructor() {
+        super();
+        this.state = {
+            option : String,
+            station : String
+        }
+    }
+
+    stationChanged = (event) => {
+        this.station = event.target.value
+        console.log('El valor de station es: ' + this.station)
+    };
+
+
     render() {
         return(
             <div>
                 <div>&nbsp;</div>
-                CT
+                <text className={'Text'}>Consulta de: </text>
                 <div>&nbsp;</div>
-                <Divider variant={'middle'}/>
-                <Divider variant={'middle'}/>
+                <div>
+                    <Select onChange={this.stationChanged}
+                            value={this.option}>
+                        <MenuItem value={''}><em>Seleccione una estacion</em></MenuItem>
+                        <MenuItem value={'Estacion ejemplo 1'}>Fecha</MenuItem>
+                        <MenuItem value={'Estacion ejemplo 2'}>ID</MenuItem>
+                        <MenuItem value={'Estacion ejemplo 3'}>Estacion</MenuItem>
+                    </Select>
+                </div>
+
+                <div>&nbsp;</div>
+                <div>&nbsp;</div>
+
+                <div>
+                    <form>
+                        <TextField
+                            id="date"
+                            label="Fecha de Viaje"
+                            type="date"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <TextField
+                            color={'primary'}
+                            required={true}
+                            placeholder={"ID de usuario"}
+                            id="txtStation"
+                            variant="outlined"
+                            onChange={this.userChanged}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Select onChange={this.stationChanged}
+                                value={this.station}>
+                            <MenuItem value={''}><em>Seleccione una estacion</em></MenuItem>
+                            <MenuItem value={'Estacion ejemplo 1'}>Estacion 1</MenuItem>
+                            <MenuItem value={'Estacion ejemplo 2'}>Estacion 2</MenuItem>
+                            <MenuItem value={'Estacion ejemplo 3'}>Estacion 3</MenuItem>
+                        </Select>
+                    </form>
+                    <div>
+                        <div>&nbsp;</div>
+                        <Button
+                            color={"primary"}
+                            variant="contained">
+                            Realizar consulta
+                        </Button>
+                        <div>&nbsp;</div>
+                        <Card>
+                            <CardContent>
+                                <Typography align='center' variant='h6' color="textSecondary">
+                                    {'Fecha: En este dia hay x viajes.'}
+                                    <br />
+                                    {'ID: este usuario tiene 23189312 viajes activos.'}
+                                    <br />
+                                    {'Estacion: por esta estacion pasan 123 trenes.'}
+                                    <br />
+                                    {'Mucho Texto'}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         );
     }
